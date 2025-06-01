4572ad0f2dece3b5b0f3bbea28d3e0c88833a8f8
ItsFlowwey <73145114+GauntletGames-2086@users.noreply.github.com>
2025-06-01 10:58:16 -0800

    Some Game class globals

--- lsp_def/vanilla/classes/game.lua
+++ lsp_def/vanilla/classes/game.lua
@@ -7,6 +7,211 @@ Game.__index = Game
 Game.super = Object
 
 G = Game()
+
+---@type string
+--- Balatro version
+G.VERSION = ""
+
+---@type boolean
+--- Include the main menu "Quit" button
+G.F_QUIT_BUTTON = true
+
+---@type boolean
+--- Completely skup the tutorial on fresh save
+G.F_SKIP_TUTORIAL = false
+
+---@type boolean
+--- Completely skip the tutorial on fresh save
+G.F_BASIC_CREDITS = false
+
+---@type boolean
+--- Remove all references to any external links (mainly for console)
+G.F_EXTERNAL_LINKS = true
+
+---@type boolean
+--- Disable debugging tool for performance of each frame
+G.F_ENABLE_PERF_OVERLAY = false
+
+---@type boolean
+--- Disables all 'run' saving
+G.F_NO_SAVING = false
+
+---@type boolean 
+--- Force mute all sounds
+G.F_MUTE = false
+
+---@type boolean 
+--- Have sound in a separate thread entirely - if not sounds will run on main thread
+G.F_SOUND_THREAD = true
+
+---@type boolean 
+--- Let the player change their video settings
+G.F_VIDEO_SETTINGS = true
+
+---@type boolean
+--- Call to Action video for the Demo - keep this as false
+G.F_CTA = false
+
+---@type boolean 
+--- Extra debug information on screen and in the console
+G.F_VERBOSE = true
+
+---@type boolean
+--- Include HTTP scores to fetch/set high scores
+G.F_HTTP_SCORES = false
+
+---@type number
+--- Add rumble to the primary controller - adjust this for amount of rumble
+G.F_RUMBLE = nil
+
+---@type boolean
+--- Send Crash reports over the internet
+G.F_CRASH_REPORTS = false
+
+---@type boolean
+--- Hard crash without error message screen
+G.F_NO_ERROR_HAND = false
+
+---@type boolean
+--- Swapping button pips for A and B buttons (mainly for switch)
+G.F_SWAP_AB_PIPS = false
+
+---@type boolean
+--- Swapping button function for A and B buttons (mainly for switch)
+G.F_SWAP_AB_BUTTONS = false
+
+---@type boolean
+--- Swapping button function for X and Y buttons (mainly for switch)
+G.F_SWAP_XY_BUTTONS = false
+
+---@type boolean
+--- Disables Achievements
+G.F_NO_ACHIEVEMENTS = false
+
+---@type string
+--- If a username is required to be displayed in the main menu, set this value to that name
+G.F_DISP_USERNAME = ""
+
+---@type boolean
+--- Disables language selection - only in english
+G.F_ENGLISH_ONLY = false
+
+---@type boolean
+--- Replaces back/select with a "guide" button
+G.F_GUIDE = false
+
+---@type boolean 
+--- Call to action for Jan demo
+G.F_JAN_CTA = false
+
+---@type boolean 
+--- Hide the game objects when paused
+G.F_HIDE_BG = false
+
+---@type boolean
+--- Use "trophy" terminology instead of "achievement"
+G.F_TROPHIES = false
+
+---@type boolean
+--- Use PS4 glyphs instead of PS5 glyphs for PS controllers
+G.F_PS4_PLAYSTATION_GLYPHS = false
+
+---@type boolean
+--- Uses `G.CLIPBOARD` for copy/paste rather than `love.system`
+G.F_LOCAL_CLIPBOARD = false
+
+---@type number
+--- Timer between saves
+G.F_SAVE_TIMER = 30
+
+---@type boolean 
+--- Enables mobile UI
+G.F_MOBILE_UI = false
+
+---@type boolean
+--- Hides beta languages
+G.F_HIDE_BETA_LANGS = false
+
+---@type number
+--- Number used for generating seeds, starts as `os.time()`
+G.SEED = 0
+
+---@type number
+--- Tile size
+G.TILESIZE = 20
+
+---@type number
+--- Tile scale
+G.TILESCALE = 3.65
+
+---@type number
+--- Tile width
+G.TILE_W = 20
+
+---@type number
+--- Tile height
+G.TILE_H = 11.5
+
+---@type number
+--- Draw hash buffer
+G.DRAW_HASH_BUFF = 2
+
+---@type number
+--- Default card width
+G.CARD_W = 2.4*35/41
+
+---@type number 
+--- Default card height
+G.CARD_H = 2.4*47/41
+
+---@type number
+--- Highlight height
+G.HIGHLIGHT_H = 0.2*G.CARD_H
+
+---@type number
+--- Collision buffer
+G.COLLISION_BUFFER = 0.05
+
+---@type number
+--- Pitch modifier
+G.PITCH_MOD = 1
+
+---@type table
+--- Table of timers
+G.TIMERS = {}
+
+---@type table
+--- Table of fame counters
+G.FRAMES = {}
+
+---@type table
+--- Table of factors used for moving objects
+G.exp_times = {}
+
+---@type table
+--- Table of game settings
+G.SETTINGS = {}
+
+---@type table
+--- Table with information about collabs
+G.COLLABS = {}
+
+---@type table
+--- Unused in vanilla. Table of metrics, saved as 
+G.METRICS = {}
+
+---@type table[]
+--- Array of tables corresponding to profiles.
+G.PROFILES = {}
+
+---@type table<string, number>
+--- Table of game states
+G.STATES = {}
+
+---@type table<string, number>
+--- Table of game stages
+G.STAGES = {}
+
 ---@type Color[]
 --- Table of colors. Contains some generic colors.
 G.C = {}
