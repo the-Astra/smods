930cd2e426342dca238e51927b04826e12d2eb68
WilsontheWolf <git@shorty.systems>
2026-08-14 03:23:13 -0600

    fix: upscale dpi dependant

--- src/game_object.lua
+++ src/game_object.lua
@@ -493,7 +493,7 @@ Set `prefix_config.key = false` on your object instead.]]):format(obj.key), obj.
                 local image = love.graphics.newImage(file_data)
                 local newScale = texture_scaling/other_scale
                 local w, h = image:getWidth(), image:getHeight()
-                local nw, nh = w * newScale, h * newScale
+                local nw, nh = love.window.fromPixels(w * newScale, h * newScale)
                 local canvas = love.graphics.newCanvas(nw, nh)
                 image:setFilter("nearest", "nearest")
                 love.graphics.setCanvas(canvas)
--- version.lua
+++ version.lua
@@ -1,1 +1,1 @@
-return "1.0.0~BETA-2013d-STEAMODDED"
+return "1.0.0~BETA-2013e-STEAMODDED"
