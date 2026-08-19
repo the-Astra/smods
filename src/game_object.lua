35ef23df456ac711ab578fc37c26b8fb6677deee
WilsontheWolf <git@shorty.systems>
2026-08-18 22:55:02 -0600

    apparently thats intentional

--- src/game_object.lua
+++ src/game_object.lua
@@ -510,7 +510,6 @@ Set `prefix_config.key = false` on your object instead.]]):format(obj.key), obj.
             end
             self.image = love.graphics.newImage(self.image_data,
                 { mipmaps = true, dpiscale = G.SETTINGS.GRAPHICS.texture_scaling })
-            self.image:setFilter("nearest", "nearest")
             self.columns = self.image:getWidth() / self.px
             self.rows = self.image:getHeight() / self.py
             G[atlas_table_map[self.atlas_table]][self.key_noloc or self.key] = self
--- version.lua
+++ version.lua
@@ -1,1 +1,1 @@
-return "1.0.0~BETA-2018b-STEAMODDED"
+return "1.0.0~BETA-2018c-STEAMODDED"
